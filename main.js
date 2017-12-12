f8726dd7e10343a1f6070888819783823be665a8
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-12-13 00:41:41 +0800

    Added blockchain validation.

--- main.js
+++ main.js
@@ -10,4 +10,11 @@ let jojocoin = new Blockchain();
 jojocoin.addBlock(new Block(1, now_utc, { amount: 123.45 }));
 jojocoin.addBlock(new Block(2, now_utc, { amount: 678.90 }));
 
+console.log("Is blockchain valid? " + jojocoin.isChainValid());
+console.log(JSON.stringify(jojocoin, null, 4));
+
+//try to simulate invalid chain
+jojocoin.chain[1].data = { amount: 999.99 };
+jojocoin.chain[1].hash = jojocoin.chain[1].calculateHash();
+console.log("\n\nIs blockchain valid? " + jojocoin.isChainValid());
 console.log(JSON.stringify(jojocoin, null, 4));
